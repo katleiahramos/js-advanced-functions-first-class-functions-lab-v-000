e9782b90a1bdd65df5baab1a69842a24c377f7a6
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-12 20:33:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,7 @@ const createFareMultiplier = function (int) {
 const fareDoubler = createFareMultiplier(2)
 
 const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function () {
+  
+}
